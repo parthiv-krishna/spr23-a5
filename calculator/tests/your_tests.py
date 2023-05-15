0153cb757d3ec508187ff739bf4bc96ebce9028f
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2023-05-15 16:56:35 -0700

    additional calculator tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,15 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
+### EXAMPLE TESTS
+# Checks that the program outputs "-3" for an input of "34 - 37"
+assert run("34 - 37").output == "-3"
+# Checks that the program outputs "-2" for an input of "-4 / 2"
+assert run("-4 / 2").output == "-2"
+# Checks that the program exists successfully (no error) for input "5 / 7"
+assert run("5 / 7").exit_status == 0
+# Checks that the program fails (correctly errors) for input "3 / 0"
+assert run("3 / 0").exit_status != 0
 ###
 
 print("All tests passed!")
